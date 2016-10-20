e76910490feb4bd07b7e786ce2911cf844545aa3
Lisa Bronwyn <e.bronwyn@gmail.com>
2016-10-20 13:29:57 -0700

    revised js code

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-		var quoteSource=[
+$(document).ready(() => {
+		const quoteSource=[
 		{
 				quote: "Have you ever retired a human by mistake?",
 				name:"Rachael"
@@ -151,26 +151,26 @@ $(document).ready(function(){
 
 	];
 
-$('#quoteButton').click(function(evt){
+$('#quoteButton').click(evt => {
 			//define the containers of the info we target
-			var quote = $('#quoteContainer p').text();
-			var quoteGenius = $('#quoteGenius').text();
+			const quote = $('#quoteContainer p').text();
+			const quoteGenius = $('#quoteGenius').text();
 			//prevent browser's default action
 			evt.preventDefault();
 			//getting a new random number to attach to a quote and setting a limit
-			var sourceLength = quoteSource.length;
-			var randomNumber= Math.floor(Math.random()*sourceLength);
+			const sourceLength = quoteSource.length;
+			const randomNumber= Math.floor(Math.random()*sourceLength);
 			//set a new quote
 			for(i=0;i<=sourceLength;i+=1){
-			var newQuoteText = quoteSource[randomNumber].quote;
-			var newQuoteGenius = quoteSource[randomNumber].name;
+			const newQuoteText = quoteSource[randomNumber].quote;
+			const newQuoteGenius = quoteSource[randomNumber].name;
 			//console.log(newQuoteText,newQuoteGenius);
-      var timeAnimation = 500;
-      var quoteContainer = $('#quoteContainer');
+      const timeAnimation = 500;
+      const quoteContainer = $('#quoteContainer');
       //fade out animation with callback
-      quoteContainer.fadeOut(timeAnimation, function(){
+      quoteContainer.fadeOut(timeAnimation, () => {
         quoteContainer.html('');
-				quoteContainer.append('<p>'+newQuoteText+'</p>'+'<p id="quoteGenius">'+'-								'+newQuoteGenius+'</p>');
+				quoteContainer.append(`<p>${newQuoteText}</p><p id="quoteGenius">-								${newQuoteGenius}</p>`);
 
         //fadein animation.
         quoteContainer.fadeIn(timeAnimation);
